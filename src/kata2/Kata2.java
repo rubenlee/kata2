7334f65e2e68cc8eb22e84d5b6fdf760ef001a4d
usuario <usuario@10.22.143.54>
2016-10-07 08:27:43 +0100

    kata v4 ¿cutre? 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,11 +5,12 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
-        int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        //Integer[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        String[] data = {"Ana","Luis","Ana","Rosa","Rosa"};
         Histogram histo = new Histogram(data);
-        Map<Integer,Integer> histogram = histo.getHistogram();   
+        Map<String,Integer> histogram = histo.getHistogram();   
 
-        for (int key : histogram.keySet()) {
+        for (String key : histogram.keySet()) {
             System.out.println(key + "~>" + histogram.get(key));
         }
     }
