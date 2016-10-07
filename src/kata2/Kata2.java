f03e4fa5f2d46630430928bd33696d26bb8193ba
usuario <usuario@10.22.143.54>
2016-10-07 08:17:51 +0100

    kata v3 cutre 3 ¿maybe?

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,10 +6,9 @@ import java.util.Map;
 public class Kata2 {
     public static void main(String[] args) {
         int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-        Map<Integer,Integer> histogram = new HashMap<>();   
-        for (int i : data) {
-            histogram.put(i, histogram.containsKey(i)?histogram.get(i)+1:1);
-        }
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogram = histo.getHistogram();   
+
         for (int key : histogram.keySet()) {
             System.out.println(key + "~>" + histogram.get(key));
         }
